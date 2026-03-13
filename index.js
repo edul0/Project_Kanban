86de146e8389e817248c0a310a5a4e3ae9711b98
EduardoL <eduardolesse0@gmail.com>
2026-03-13 06:44:13 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Meu Kanban Local-First</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div id="kanban-board" class="board-container">
+        </div>
+
+    <script src="script.js"></script>
+    <script>
+        // Inicializa o tabuleiro assim que a página carregar
+        window.onload = renderBoard;
+    </script>
+</body>
+</html>
